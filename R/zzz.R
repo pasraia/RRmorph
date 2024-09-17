7c2cbb6770070c3b6f8dfb7653d3ec5971378c82
pasraia <30688520+pasraia@users.noreply.github.com>
2024-09-17 12:52:22 +0200

    RRmorph 0.0.1

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("The package was first published in Melchionna et al. 2024 - Communications Biology.
+To replicate the analyses presented in the paper follow the link:
+https://github.com/pasraia/Supporting-Material-Melchionna-et-al.-2024")
+}
